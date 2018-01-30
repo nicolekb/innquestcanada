8e324c89e06236e6fe776476e59b8a9edabd8e7c
nicolekb <nicolekbuloran@gmail.com>
2018-01-30 14:29:13 -0700

    setup, installed jquery, installed bootstrap, installed sass

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * innquestcanada Theme Customizer
+ *
+ * @package innquestcanada
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function innquestcanada_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'innquestcanada_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'innquestcanada_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'innquestcanada_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function innquestcanada_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function innquestcanada_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function innquestcanada_customize_preview_js() {
+	wp_enqueue_script( 'innquestcanada-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'innquestcanada_customize_preview_js' );
